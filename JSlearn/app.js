6e105c52b3200398a9764458a26ec098341a218b
Leonid <scorpion23997@gmail.com>
2020-12-25 15:27:16 +0200

    Initial commit

---
+++ JSlearn/app.js
@@ -0,0 +1,10 @@
+//1 - Переменные
+// var не использовать - плохой тон
+const firstName = 'Leonid'
+let age = 20
+console.log(age)
+
+
+//2 - мутирование
+console.log('имя человека '+ firstName + ', а возвраст '+age)
+
