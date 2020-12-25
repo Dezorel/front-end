58f027c455a57546e38e29685ac63c53a1af3f9f
Leonid <scorpion23997@gmail.com>
2020-12-26 00:51:44 +0200

    string

---
+++ JSlearn/03_string.js
@@ -0,0 +1,43 @@
+// string
+/*
+const name = 'leonid'
+const age = 20
+
+function getAge() {
+    return age
+}
+/*
+ */
+/*
+//const output = 'Привет, я - ' + name +' и мой возвраст ' + age
+const output = `Привет, я - ${name} и мой возвраст ${getAge()}`
+console.log(output)
+*/
+/*
+const output =`
+    <div class="container">
+    <h1>This is H1</h1>
+    <p>This is paragraph</p>
+</div>
+`
+console.log(output)
+*/
+function  logPerson(s, n, age){
+    //console.log(s, n, age)
+    if(age < 0){
+        age = 'Ещё не родился'
+    }
+    //return 'Info about person'
+
+    //return `${s[0]}${n}${s[1]}${age}${s[2]}`
+}
+const pName = 'Vlad'
+const pAge = 26
+ //const output = logPerson`Имя: ${pName}, Возвраст ${pAge}!`
+console.log(output)
+
+
+
+
+
+
