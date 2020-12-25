7927488526025c661b323cf64659f7873ea70020
Leonid <scorpion23997@gmail.com>
2020-12-25 15:53:24 +0200

    4 - primitive type

--- JSlearn/app.js
+++ JSlearn/app.js
@@ -2,7 +2,7 @@
 // var не использовать - плохой тон
 const firstName = 'Leonid'
 let myAge = 20
-console.log(myAge)
+//console.log(myAge)
 
 
 //2 - мутирование
@@ -15,11 +15,28 @@ console.log('имя человека '+ firstName + ', а возвраст '+myA
 */
 
 
-//3 - Операторы
+//3 - Операторы всё по стандартам обычных языков
+/*
 const currentYear = 2020;
 const bearthYear = 2000;
 const age = currentYear - bearthYear
 
+console.log( age )
+*/
 
+
+// 4 - Типы данных(примитивные)
+/*
+const isProgrammer = true
+const name = 'Leonid'
+const age = 20
+let x
+
+console.log(typeof isProgrammer)
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof x)
+console.log(null)
+*/
 
 
