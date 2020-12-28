234df35b762bb5b4ba2c86160a6ff1b6ce9bded4
Leonid <scorpion23997@gmail.com>
2020-12-28 21:51:54 +0200

    finish search in site

--- Vue/searchInSite/script.js
+++ Vue/searchInSite/script.js
@@ -1,24 +1,15 @@
+class Post {
+    constructor(title, description) {
+        this.title = title;
+        this.description = description;
+    }
+}
+
 Vue.component('card-item',{
     props: {
         name: Object,
-
     },
-    computed: {
-        foundItemInList(){
-            for(let i=0; i<6; i++)
-            {
-                if(!name.text)
-                    return {
-                        display: 'block'
-                    }
-
-            }
-            return {
-                display: 'none'
-            }
-        }
-    },
-    template: '<div class="col mb-4 mt-4">\n' +
+    template: '<div class="col mb-4 mt-4"  >\n' +
         '            <div class="card">\n' +
         '                <div class="card-body">\n' +
         '                    <h5 class="card-title">{{name.text}}</h5>\n' +
@@ -32,27 +23,40 @@ Vue.component('card-item',{
 var app = new Vue({
     el: '#app',
     data: {
-        foundItem: '',
-        productsList:[
-            {text: 'Картошка'},
-            {text: 'Творог'},
-            {text: 'Томаты'},
-            {text: 'Торты'},
-            {text: 'Морковь'},
-            {text: 'Макароны'},
-        ],
-        descriptionList: [
-            {text: 'Это Картошка возрадуйся! Она может стать твоей!'},
-            {text: 'Это Творог возрадуйся! Он может стать твоим!'},
-            {text: 'Это Томаты возрадуйся! Они могут стать твоими!'},
-            {text: 'Это Торт возрадуйся! Он могжет стать твоим!'},
-            {text: 'Это Морковь возрадуйся! Она может стать твоей!'},
-            {text: 'Это Макароны возрадуйся! Они могут стать твоими!'},
-
+        search: '',
+        postList:[
+            new Post(
+                'Картошка',
+                'Это же Картошка. В считанные секунды может стать твоей',
+            ),
+            new Post(
+                'Творог',
+                'Это же Творог. В считанные секунды может стать твоим',
+            ),
+            new Post(
+                'Томаты',
+                'Это же Томаты. В считанные секунды могут стать твоими',
+            ),
+            new Post(
+                'Торт',
+                'Это же Торт. В считанные секунды может стать твоим',
+            ),
+            new Post(
+                'Морковь',
+                'Это же Морковь. В считанные секунды может стать твоей',
+            ),
+            new Post(
+                'Макароны',
+                'Это же Макароны. В считанные секунды могут стать твоими',
+            ),
         ]
     },
-    methods:{
-
+    computed: {
+        filteredList() {
+            return this.postList.filter(post => {
+                return post.title.toLowerCase().includes(this.search.toLowerCase())
+            })
+        }
     }
 })
 
