1b7964eb7e2f272c659aff7da52436c8ef2761e6
Leonid <scorpion23997@gmail.com>
2020-12-28 19:09:15 +0200

    components vue
    learn vue components and create my todo list with input

--- Vue/someThinkGood.js
+++ Vue/someThinkGood.js
@@ -1,12 +1,34 @@
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>  {{ todo.text }}   </li>'
+})
+
+Vue.component('my-todo',{
+    props: ['myTodo'],
+    template: '<li> {{ myTodo.todoText }} </li>'
+})
+
 let app = new Vue({
     el: '.app',
     data: {
         message: 'Вы загрузили эту страницу: ' + new Date().toLocaleString(),
-        text: 'In this area can be your text'
+        text: 'In this area can be your text',
+        newTextTodo: '',
+        groceryList: [
+            {id:0 , text:'Купить овощи'},
+            {id:1 , text:'Помыть посуду'},
+            {id:2 , text:'Выучить VUE'},
+            {id:3 , text:'Выспаться'}
+        ],
+        todoList:[],
     },
     methods: {
         reverseMessage: function () {
             this.text = this.text.split('').reverse().join('')
+        },
+        createTodo: function (){
+            this.todoList.push({ todoText: this.newTextTodo })
+            this.newTextTodo =''
         }
     }
 })
