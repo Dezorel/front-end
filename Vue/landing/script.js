a85fa022517827cf0758e8a4615d55ba33b27680
Leonid <scorpion23997@gmail.com>
2020-12-30 19:39:36 +0200

    update

--- Vue/landing/script.js
+++ Vue/landing/script.js
@@ -1,36 +1,70 @@
 class ButtonCheckBox {
-    constructor(title) {
+    constructor(title, type) {
         this.title = title
+        this.type = type
         this.isClick = false
     }
     changeStyle(){
         this.isClick = !this.isClick
+        this.write()
+    }
+    write(){
+        if(this.isClick){
+            app.message += ' '+this.type +':'+ this.title
+        }
+        else {
+            if(app.message.indexOf(this.title)){
+                app.message = app.message.replace(this.type +':'+ this.title, '');
+            }
+        }
     }
 }
-
 let app = new Vue({
     el: '#up',
     data: {
-       //isClick: false
-        buttonList:[
+        message: '',
+        todoButtonList:[
             new ButtonCheckBox(
                 'Лэндинг',
+                'Работа'
             ),
             new ButtonCheckBox(
                 'Сайт под ключ',
+                'Работа'
             ),
             new ButtonCheckBox(
                 'Мобильное приложение',
+                'Работа'
             ),
             new ButtonCheckBox(
                 'Проконсультироваться',
+                'Работа'
             ),
-        ]
+        ],
+        priceList:[
+            new ButtonCheckBox(
+                'Минимальный',
+                'Бюджет'
+            ),
+            new ButtonCheckBox(
+                '50$',
+                'Бюджет'
+            ),
+            new ButtonCheckBox(
+                '80$',
+                'Бюджет'
+            ),
+            new ButtonCheckBox(
+                '100$',
+                'Бюджет'
+            ),
+            new ButtonCheckBox(
+                'Неограниченный',
+                'Бюджет'
+            ),
+        ],
     },
     methods:{
-        clickForm(){
-            console.log(this.isClick)
-            return  (this.isClick = !this.isClick)
-        },
+
     }
 })
