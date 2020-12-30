28d7d2c546282ab505853f8a19216b34af363990
Leonid <scorpion23997@gmail.com>
2020-12-30 12:23:01 +0200

    calculator

--- Vue/calculator/calculatorScript.js
+++ Vue/calculator/calculatorScript.js
@@ -1,6 +1,56 @@
 let app = new Vue({
     el:'#app',
     data:{
-
-    }
+        currentNumber: '',
+        operator: null,
+        previous: null,
+        operatorClicked: false,
+    },
+    methods:{
+        setPrevious(){
+            this.previous = this.currentNumber
+            this.operatorClicked = true
+        },
+        append(num){
+            if(this.operatorClicked){
+                this.currentNumber =''
+                this.operatorClicked = false
+            }
+            this.currentNumber = `${this.currentNumber}${num}`
+        },
+        clear(){
+          this.currentNumber = ''
+        },
+        percent(){
+          this.currentNumber /=100
+        },
+        division(){
+          this.operator = (a,b) => a / b;
+            this.setPrevious()
+        },
+        multiplication(){
+          this.operator = (a,b) => a * b;
+            this.setPrevious()
+        },
+        plus(){
+          this.operator = (a,b) => a + b;
+            this.setPrevious()
+        },
+        minus(){
+          this.operator = (a,b) => a - b;
+            this.setPrevious()
+        },
+        equal() {
+            this.currentNumber = `${this.operator(
+                parseFloat(this.currentNumber),
+                parseFloat(this.previous)
+            )}`
+            this.previous = null
+        },
+        dot(){
+            if(this.currentNumber.indexOf('.') == '-1'){
+                this.append('.')
+            }
+        }
+    },
 })
