c4b2bc4389d2af39f1af12654d70dbe1b1cab7c0
Leonid <scorpion23997@gmail.com>
2021-01-06 19:41:42 +0200

    tested simple and loops components

--- Vue/componentsTest/script.js
+++ Vue/componentsTest/script.js
@@ -1,8 +1,47 @@
+Vue.component('test',{
+    props: {
+        title: String,
+        subtitle: String,
+        text: String,
+        isClick: Boolean,
+    },
+    template: '<div class="card" style="width: 18rem; background-color: ">\n' +
+        '  <div class="card-body">\n' +
+        '    <h5 class="card-title">{{title}}</h5>\n' +
+        '    <h6 class="card-subtitle mb-2 text-muted">{{subtitle}}</h6>\n' +
+        '    <p class="card-text">{{text}}</p>\n' +
+        '  </div>\n' +
+        '</div>',
+})
+
+class Post{
+    constructor(title, subtitle, text) {
+        this.title = title
+        this.subtitle = subtitle
+        this.text = text
+    }
+}
 
 let app = new Vue({
     el: '#thisApp',
     data: {
-   
+        list:[
+            new Post(
+                'this',
+                'is',
+                'teeesst'
+            ),
+            new Post(
+                'other',
+                'text',
+                'her'
+            ),
+            new Post(
+                '1',
+                '2',
+                '3'
+            )
+        ]
     },
     methods: {
 
