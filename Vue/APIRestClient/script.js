9de082239010dfcf00acb9c01f2a03055bcfa0f1
Leonid <scorpion23997@gmail.com>
2021-01-09 23:56:39 +0200

    create form

--- Vue/APIRestClient/script.js
+++ Vue/APIRestClient/script.js
@@ -1,1 +1,16 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js'
+
+new Vue({
+    el:'#app',
+    data(){
+        return {        //возвращаем объект который является моделями для нашего приложения
+            form:{      //Создаю объект форм, у него 2 контрола
+                name: '',
+                value: '',
+            }
+        }
+    },
+    methods:{
+
+    },
+})
