07eef3c069eb00967b38bd826c1927f5bee4e059
Leonid <scorpion23997@gmail.com>
2021-01-11 20:48:24 +0200

    create event buttons

--- Vue/APIRestClient/script.js
+++ Vue/APIRestClient/script.js
@@ -1,13 +1,17 @@
 
-new Vue({
+
+
+let app = new Vue({
     el:'#app',
     data(){
         return {        //возвращаем объект который является моделями для нашего приложения
             form: {      //Создаю объект форм, у него 2 контрола
                 name: '',
-                value: ''
+                value: '',
             },
-            contacts: []
+            contacts: [
+                {id: 1, name: 'Leonid', value: '777-777-7', marked: false}
+            ]
         }
     },
     methods:{
@@ -16,6 +20,13 @@ new Vue({
             this.contacts.push({...contact, id: Date.now() })
             console.log(this.contacts)
             this.form.name = this.form.value = ''
+        },
+        markedContact(id){
+            const contact = this.contacts.find(c => c.id === id)
+            contact.marked = true
+        },
+        deletecontact(){
+
         }
     },
 })
